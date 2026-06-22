d2ff18f4a47430f8fa2606e92ed5544ebe037b40
Rimu Eirnarn <Rimuru720@proton.me>
2026-06-23 05:44:56 +0700

    some synthetic stuff

--- js/dom.mjs
+++ js/dom.mjs
@@ -1,3 +1,12 @@
+/**
+ * @typedef CommonProps
+ *
+ * @prop {string} [class]
+ * @prop {Partial<CSSStyleDeclaration>} [style]
+ * @prop {Record<string, string>} [dataset]
+ * @prop {Record<string, EventListener|({handler: EventListener} & EventListenerOptions)} [on]
+ */
+
 
 /**
  * Shorthand for document.query
@@ -12,14 +21,34 @@ export function query(selector) {
 
 /**
  * Tiny helper for inline "HTML"
- * @param {string} tag
- * @param {Object.<string, any>} props
- * @param  {...HTMLElement|Node|string} children
- * @returns {HTMLElement}
+ *
+ * @template {keyof HTMLElementTagNameMap} K
+ *
+ * @param {K} tag
+ * @param {CommonProps & Partial<HTMLElementTagNameMap[K]>} [props]
+ * @param  {...Node|string} children
+ * @returns {HTMLElementTagNameMap[K]}
  */
 export function e(tag, props, ...children) {
   const element = document.createElement(tag)
+  const { on, class: className, style, dataset, remaining } = props
+
   Object.assign(element, props)
+  if (className) element.className = className;
+  if (style) Object.assign(element.style, style);
+  if (dataset) Object.assign(element.dataset, dataset);
+
+  if (on) {
+    for (const [event, handler] of Object.entries(on)) {
+      if (typeof handler === 'function')
+        element.addEventListener(event, handler);
+      if (typeof handler === 'object') {
+        const { handler: callable, handler_props } = handler;
+        element.addEventListener(event, callable, handler_props)
+      }
+    }
+  }
+
   for (const child of children) {
     element.append(child)
   }
@@ -27,11 +56,14 @@ export function e(tag, props, ...children) {
 }
 
 /**
- * @typedef {(props?: Object.<string, any>, ...children: (HTMLElement|Node|string)[])} Builder
+ * @callback Builder
+ * @param {Object} [props]
+ * @param {...Node|string} children
+ * @returns {HTMLElement}
  */
 
 /**
- * @type {Object.<string, Builder>}
+ * @type {Record<string, Builder>}
  */
 export const html = new Proxy({}, {
   /**
